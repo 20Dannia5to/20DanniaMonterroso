8101fce7f22b10602047a54bb58897e1f193784e
20Dannia5to <162483888+20Dannia5to@users.noreply.github.com>
2024-03-11 22:19:20 -0600

    Update Par.java

--- Par/src/main/java/com/mycompany/par/Par.java
+++ Par/src/main/java/com/mycompany/par/Par.java
@@ -3,24 +3,24 @@ package com.mycompany.Par;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-import java.util.Scanner;
+import java.util.Scanner; // Importar la clase Scanner
 
 
 /**
  *
  * @author Hns. Monterroso
  */
-public class Par {
+public class Par { // Define una clase publica llamada Par
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese un valor: ");
-        int numero = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in); //Crea una instancia de Scanner para leer lo que el usuario le indique
+        System.out.println("Ingrese un valor: "); //Solicita al usuario que ingrese un valor de tipo entero
+        int numero = scanner.nextInt(); // Lee el valor entero que el usuario ingreso
 
-        if (numero % 2 == 0) {
-            System.out.println(numero + " es par.");
+        if (numero % 2 == 0) { // Comprueba si el numero ingresado es par 
+            System.out.println(numero + " es par."); // Si es par, muestra el mensaje "par"
         } else {
-            System.out.println(numero + " es impar.");
+            System.out.println(numero + " es impar."); // Si no es par muestra un mensaje indicando que es "impar"
         }
     }
 }
